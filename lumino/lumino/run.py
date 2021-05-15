25749932e971b2284fab5a496dceaf2ddd2c193a
Jimmy Red <putnamjm.design@gmail>
2021-05-14 23:37:53 -0400

    #12 - design rotor language

--- lumino/lumino/run.py
+++ lumino/lumino/run.py
@@ -8,26 +8,16 @@
 
 ##########
 ##
-## gra-afch controller
+## lumino controller
 ##
 ###########
 
-import time
 import json
-import ncs31x
-import rotor
 
 VERSION = '0.0.1'
 
-with open('./gra-afch.conf', 'r') as file:
+with open('./lumino.conf', 'r') as file:
     conf_dict = json.load(file)
-    ncs31x.ncs31x(conf_dict)
     print(VERSION)
     print(json.dumps(conf_dict))    
 
-rotor.display_date()
-time.sleep(1)
-ncs31x.blank()
-while True:
-    rotor.display_time()
-    time.sleep(1)

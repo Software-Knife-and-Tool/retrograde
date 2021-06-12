8c549b729fcdb000dc3810d37febebfe474d0013
Jim Dandy <putnamjm.design@gmail.com>
2021-06-12 10:16:22 -0400

    #118 - add service user

--- retrograde/release/run.sh
+++ retrograde/release/run.sh
@@ -3,4 +3,4 @@ BASE=/opt/retrograde
 BIN=$BASE/venv/bin
 PACKAGES=$BASE/retro:$BASE/retro/modules:$BASE/venv/lib/python3.7/site-packages
 
-env "PATH=$BIN" "PYTHONPATH=$PACKAGES" python3 app.py
+env "PATH=$BIN" "PYTHONPATH=$PACKAGES" python3 $BASE/retro/app.py
